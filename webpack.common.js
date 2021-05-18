3c2e260983db40613be4956c7a67d7447d276760
Nyar.Gem <christineodegi.sd@gmail.com>
2021-05-18 13:01:03 -0600

    Initial Commit

---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js" //if you want to seperate vendor code from your code
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      }
+    ]
+  }
+};
